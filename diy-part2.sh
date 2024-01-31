24df972c623860431ea7a3339210642be9858882
Lai-xi <56962531+Lai-xi@users.noreply.github.com>
2024-01-31 10:32:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 切换内核
-sed -i 's/6.1/5.15/g' target/linux/x86/Makefile
-sed -i 's/6.1/5.15/g' target/linux/rockchip/Makefile
+# sed -i 's/6.1/5.15/g' target/linux/x86/Makefile
+# sed -i 's/6.1/5.15/g' target/linux/rockchip/Makefile
